cc6a9c110fae4413fa8da7d05684292b2fa919c6
Nguyen Hong Nhung <hongnhungnguyen2602@gmail.com>
2014-06-25 02:14:36 +0700

    changeImg/title moveBox creatElement onfocus&onblurEmail changeInlineStyle

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
  * Created by hongnhung on 23/06/2014.
  */
 var container = document.getElementById("container");
-//container.setAttribute("align","right");
+container.setAttribute("align","left");
 
 //var title = document.getElementById(title);
 //console.log(title.innerHTML);
@@ -10,82 +10,89 @@ var container = document.getElementById("container");
 //var arrayh2 = container.getElementsById("h2");
 //arrayh2[0].innerHTML = "How snocking";
 //arrayh2[1].innerHTML = "To be or not to be";
+
+//message
+function message() {
+        alert("You forget a page");
+}
+
+setTimeout(message, 90000);
+
+// create Element
+var newHeading2 = document.createElement("h2");
+var newParagraph = document.createElement("p");
+
+newHeading2.innerHTML = "login";
+
+var newParagraphChild = document.createTextNode("Vui long nhap du thong tin");
+newParagraph.appendChild(newParagraphChild);
+
+document.getElementById("second").appendChild(newHeading2);
+document.getElementById("second").appendChild(newParagraph);
+
+//click img
+function clickImg(){
+    var imgClick = document.getElementById("abc");
+    imgClick.onclick = function(){
+        alert("You clicked the img");
+    }
+}
+
+//change img
+var myImg = document.getElementById("abc");
+
+var imgArray = ["img/UNTITLED.jpg", "img/bb.jpg", "img/xanhla.jpg"];
+var indexImg = 0;
+
+function changeImg(){
+    myImg.setAttribute("src",imgArray[indexImg]);
+    indexImg++;
+    if(indexImg>=imgArray.length){
+        indexImg=0;
+    }
+}
+
+var handImg = setInterval(changeImg,2000);
+myImg.onclick = function(){
+    clearInterval(handImg);
+};
+
+// change title
 var myArrayTitle = ["How snocking","To be or not to be","So far so good"];
 var indexTitle = 0;
 
-function myFuncitontitle() {
+function changTitle(){
     var newTitle = myArrayTitle[indexTitle];
-    var titleElement = document.getElementsByTagId("title")
-    titleElement.innerHTML = newTitle;
+    var title = document.getElementById("title");
+    title.innerHTML=newTitle;
     indexTitle++;
-    if (indexTitle >= myArrayTitle.length) {
-        iindexTitle = 0;
+    if(indexTitle>=myArrayTitle.length){
+        indexTitle=0;
     }
+}
+setInterval(changTitle,3000);
 
+// change color and fontWeight
+document.getElementById("title").style.color="green";
+document.getElementById("title").style.fontWeight = "bold";
+document.getElementById("title").style.marginLeft = "30px";
 
-//Creating the elements
-    var newh1 = document.createElement("h1");
-    var newp = document.createElement("p");
+//check email
+var emailInput = document.getElementById("email");
 
-    newh1.innerHTML = "Login";
-
-
-    var newp1 = document.createTextNode("Toi yeu Viet Nam");
-    newp.appendChild(newp1);
-
-    document.getElementById("second").appendChild(newh1);
-//document.getElementById("second").appendChild(newp);
-
-//ClickAndLoad
-//document.onclick = function(){
-//alert("You click somewhere in the page");
-
-//}
-function clickimg() {
-        var img = document.getElementsById("abc");
-        img.onclick = function () {
-            alert("You click image ");
-        }
-
- }
-
-    var emailmain = document.getElementById("email");
-    emailmain.onfocus = function () {
-        if (emailmain.value == "Your email") {
-            emailmain.value == "";
-        }
+emailInput.onfocus = function(){
+    if(emailInput.value == "Your email"){
+            emailInput.value = "";
     }
-    emailmain.onblur = function () {
-        if (emailmain.value == "") {
-            emailmain.value = "Your email"
-        }
+};
+
+emailInput.onblur = function(){
+    if(emailInput.value == ""){
+            emailInput.value ="Enter your email";
     }
-function message() {
-        alert("You forget a page");
- }
+};
 
-setTimeout(message, 70000);
-
-    var myImg = document.getElementById("abc");
-    var imgA = ["img/UNTITLED.jpg", "img/bb.jpg", "img/xanhla.jpg"];
-    var indexImg = 0;
-
- function changeimg() {
-        myImg.setAttribute("src", imgA[indexImg]);
-        indexImg++;
-        if (indexImg >= imgA.length) {
-            indexImg = 0;
-        }
-    }
-
-
-    var hand = setInterval(changeimg, 2000);
-    myImg.onclick = function () {
-        clearInterval(hand);
-    }
-
-
-    function myFuncitoncheck() {
+/* function checkMail() {
         document.getElementById("login");
         if (document.getElementById("email").value == "") {
             document.getElementById("error").innerHTML = "Please provide at least an email";
@@ -95,12 +102,39 @@ setTimeout(message, 70000);
             return true;
         }
     }
-}
-window.onload= function(){
-    clickimg();
-    myFuncitonTitle();
-    changeimg();
-    myFuncitoncheck()
-    setInterval(myFunciton, 2000);
+
+*/
+
+// move box
+var movePos = 0;
+var moveHand ;
+
+
+function hiddenMyBox(){
+    document.getElementById("jump").style.display = "inline";
+    document.getElementById("jump").style.position ="absolute";
+    document.getElementById("jump").style.left = "0px";
+    document.getElementById("jump").style.top = "100px";
+    moveHand = setInterval(moveMyBox,50);
+
 }
 
+function moveMyBox(){
+    movePos+=5;
+    document.getElementById("jump").style.left = movePos + "px";
+
+    if(movePos > 900){
+        clearInterval(moveHand);
+        document.getElementById("jump").style.position = "";
+        document.getElementById("jump").style.left = "";
+        document.getElementById("jump").style.top = "";
+    }
+}
+
+window.onload = function(){
+    clickImg();
+    changeImg();
+    changTitle();
+    setTimeout(hiddenMyBox,5000);
+//    checkMail()
+};
